db021d0047149c8ab1b592441e5d41f2484cb408
Kayz-mann <kayodefredrickbalogun@gmail.com>
2026-08-07 08:11:39 +0100

    chore: scaffolding spring boot project for job application and review portal

---
+++ src/main/java/com/kayzmann/job/app/job/Job.java
@@ -0,0 +1,67 @@
+package com.kayzmann.job.app.job;
+
+public class Job {
+    private Long id;
+    private Long title;
+    private String description;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+
+    public Job(Long id, Long title, String description, String minSalary, String maxSalary, String location) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getMinSalary() {
+        return minSalary;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public String getLovation() {
+        return location;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(Long title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setMinSalary(String minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public void setLovation(String location) {
+        this.location = location;
+    }
+}
