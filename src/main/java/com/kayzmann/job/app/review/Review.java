5ab2da086f414324782322966e2a61415aa8542b
Kayz-mann <kayodefredrickbalogun@gmail.com>
2026-08-13 17:23:33 +0100

    add: actuator and swagger dependencies

--- src/main/java/com/kayzmann/job/app/review/Review.java
+++ src/main/java/com/kayzmann/job/app/review/Review.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 @Entity
 public class Review {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
